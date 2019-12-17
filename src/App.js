95ea37d553bdc88e07258718e22bdb75a730ecce
Matt Raible <matt.raible@okta.com>
2019-12-17 07:37:37 -0700

    Complete application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const config = {
+  issuer: 'https://dev-133320.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oa28jhpsrHyEB6vu357',
+  pkce: true
+};
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <Security {...config}>
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/implicit/callback" component={ImplicitCallback}/>
+        </Security>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Calculator.js
@@ -0,0 +1,151 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stack: ['='],
+      display: '0'
+    }
+  }
+
+  numberPressed(val) {
+    const s = this.state;
+    if (typeof s.stack[s.stack.length - 1] !== 'number') {
+      s.display = val;
+      s.stack.push(parseInt(s.display, 10));
+    } else {
+      s.display += val;
+      s.stack[s.stack.length - 1] = parseInt(s.display, 10);
+    }
+    this.setState(s);
+  }
+
+  operatorPressed(val) {
+    const s = this.state;
+    const precedenceMap = {'+': 0, '-': 0, '*': 1, '/': 1};
+    this.ensureNumber(s);
+    const precedence = precedenceMap[val];
+    let reduce = true;
+    while (reduce) {
+      let i = s.stack.length - 1;
+      let lastPrecedence = 100;
+
+      while (i >= 0) {
+        if (typeof s.stack[i] === 'string') {
+          lastPrecedence = precedenceMap[s.stack[i]];
+          break;
+        }
+        i--;
+      }
+      if (precedence <= lastPrecedence) {
+        reduce = this.reduceLast(s);
+      } else {
+        reduce = false;
+      }
+    }
+
+    s.stack.push(val);
+    this.setState(s);
+  }
+
+  equalPressed() {
+    const s = this.state;
+    this.ensureNumber(s);
+    while (this.reduceLast(s)) {}
+    s.stack.pop();
+    this.setState(s);
+  }
+
+  percentPressed() {
+    const s = this.state;
+    this.ensureNumber(s);
+    while (this.reduceLast(s)) {}
+    const result = s.stack.pop() / 100;
+    s.display = result.toString(10);
+    this.setState(s);
+  }
+
+  acPressed() {
+    const s = this.state;
+    s.stack = ['='];
+    s.display = '0';
+    this.setState(s);
+  }
+
+  cePressed() {
+    const s = this.state;
+    if (typeof s.stack[s.stack.length - 1] === 'number') { s.stack.pop(); }
+    s.display = '0';
+    this.setState(s);
+  }
+
+  ensureNumber(s) {
+    if (typeof s.stack[s.stack.length - 1] === 'string') {
+      s.stack.push(parseInt(s.display, 10));
+    }
+  }
+
+  reduceLast(s) {
+    if (s.stack.length < 4) { return false; }
+    const num2 = s.stack.pop();
+    const op = s.stack.pop();
+    const num1 = s.stack.pop();
+    let result = num1;
+    switch (op) {
+      case '+': result = num1 + num2;
+        break;
+      case '-': result = num1 - num2;
+        break;
+      case '*': result = num1 * num2;
+        break;
+      case '/': result = num1 / num2;
+        break;
+      default:
+    }
+    s.stack.push(result);
+    s.display = result.toString(10);
+    return true;
+  }
+
+  render() {
+    return (
+      <div className="calculator-container">
+        <div className="calculator">
+          <p className="display">{this.state.display}</p>
+          <div class="calculator-buttons">
+            <button className="reset-button" onClick={this.acPressed.bind(this)}>AC</button>
+            <button className="reset-button" onClick={this.cePressed.bind(this)}>CE</button>
+            <button className="operator-button" onClick={this.percentPressed.bind(this)}>%</button>
+            <button className="operator-button" onClick={this.operatorPressed.bind(this, '/')}>÷</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '7')}>7</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '8')}>8</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '9')}>9</button>
+            <button className="operator-button" onClick={this.operatorPressed.bind(this, '*')}>x</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '4')}>4</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '5')}>5</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '6')}>6</button>
+            <button className="operator-button" onClick={this.operatorPressed.bind(this, '-')}>-</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '1')}>1</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '2')}>2</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '3')}>3</button>
+            <button className="operator-button" onClick={this.operatorPressed.bind(this, '+')}>+</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '0')}>0</button>
+            <button className="number-button" onClick={this.numberPressed.bind(this, '.')}>.</button>
+            <button className="equal-button" onClick={this.equalPressed.bind(this)}>=</button>
+          </div>
+        </div>
+        <div className="calculator-stack">
+          <h4>Stack</h4>
+          <table>
+            <tbody>
+            {this.state.stack.map(el => (<tr><td>{el}</td></tr>))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
